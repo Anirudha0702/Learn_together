eaa3826e31a41e13b7ed25c4cbdd39fec7103ff8
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-01-23 17:27:02 +0530

    loginPage added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,12 @@
-import About from "../../components/about/About";
-import Cover from "../../components/cover/Cover";
-import Navbar from "../../components/navbar/Navbar";
+// import About from "../../components/about/About";
+// import Cover from "../../components/cover/Cover";
+// import Navbar from "../../components/navbar/Navbar";
+import Login from "../login/Login";
 import "./home.scss"
 const Home=()=>{
     return(
         <div className="Home">
-          <Navbar></Navbar>  
-          <Cover/>
-          <About/>
+          <Login/>
         </div>
     )
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,27 @@
+import { Facebook , LinkedIn, Person, Twitter} from "@material-ui/icons";
 import "./login.scss"
+
 const Login=()=>{
     return(
         <div className="Login">
-            
+            <div className="login_container">
+                <label htmlFor="email"><Person className="person"/> </label>
+                <input type="email" placeholder="Email" className="inpBox email"/>
+                <input type="password" placeholder="Password" className="inpBox password"/>
+                <label htmlFor="" id="rememberMe"><input type="checkbox" name="" id="chkbx" /> Remember Me</label>
+                <button className="sign_in">Sign In</button>
+
+                <div className="linkconatiner">
+                    <span>Register</span>
+                    <span>Forget password?</span>
+                </div>
+                <div className="icon_container">
+                    <Facebook className="Icon fb"/>
+                    <Twitter className="Icon twitter"/>
+                    <LinkedIn className="Icon twitter"/>
+                </div>
+            </div>
         </div>
     )
 }
+export default Login;
