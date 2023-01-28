2c88fb90cc517101ac2590ab44369012c3242d5f
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-01-28 13:34:06 +0530

    add registerPage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@
 // import Cover from "../../components/cover/Cover";
 // import Navbar from "../../components/navbar/Navbar";
 import Login from "../login/Login";
+import Register from "../register/Register";
 import "./home.scss"
 const Home=()=>{
     return(
         <div className="Home">
-          <Login/>
+          
+          <Register/>
         </div>
     )
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,26 @@
+import  "./register.scss"
+import { Facebook , LinkedIn, Person, Twitter} from "@material-ui/icons";
+
+const Register=()=>{
+    return (
+        <div className="Register">
+           <div className="Register_container">
+                <input type="email" placeholder="Email" className="inpBox email"/>
+                <div className="phoneNo">
+                <input type="text" name="" id="" className="inpBox countryCode" placeholder="+91"/>
+                <input type="text" name="" id="" inputMode="numeric" className="inpBox phone" placeholder="Phone"/>
+                </div>
+                <input type="password" placeholder="Password" className="inpBox password"/>
+                <button className="sign_up">Sign Up</button>
+                <span>Already have an account?<span>Login</span></span>
+            
+                <div className="icon_container">
+                    <Facebook className="Icon fb"/>
+                    <Twitter className="Icon twitter"/>
+                    <LinkedIn className="Icon twitter"/>
+                </div>
+           </div>
+        </div>
+    )
+}
+export default Register;
