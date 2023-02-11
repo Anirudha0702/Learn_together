3784d068818cfc93f144af3148fca425c0f460fa
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-02-11 22:28:26 +0530

    registerNewDesgin

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 // import About from "../../components/about/About";
 // import Cover from "../../components/cover/Cover";
 //import Navbar from "../../components/navbar/Navbar";
-import Login from "../login/Login"
+//import Login from "../login/Login"
+import Register from "../register/Register";
 import "./home.scss"
 const Home=()=>{
     return(
         <div className="Home">
-          <Login/>
+          <Register/>
         </div>
     )
 }
