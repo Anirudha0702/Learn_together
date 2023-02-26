adcae0669e727cbdc5476ce8715d31be22e9240b
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-02-27 00:06:25 +0530

    add about sec

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,19 @@
+import { EmojiEvents, MenuBook, Person, School } from "@material-ui/icons";
 import "./about.scss"
 const About=()=>{
     return(
         <div className="About">
-            <div className="flexItem photo"></div>
-            <div className="flexItem desc">
-                <h3>Our Journey</h3>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi atque debitis consequatur tempore ipsam nostrum laborum natus quis laboriosam corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, maiores. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, modi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur voluptates repudiandae recusandae voluptate fugit aliquid quos praesentium laboriosam, iusto itaque.
-                <button className="seeMore"><span></span>SEE MORE</button>
+            <div className="flexItem"><School className="Icon"/>
+            265+ Satisfied Students
+            </div>
+            <div className="flexItem"><Person className="Icon"/>
+            20+ Teachers
+            </div>
+            <div className="flexItem"><EmojiEvents className="Icon"/>
+            8 Awards
+            </div>
+            <div className="flexItem"><MenuBook className="Icon"/>
+            30+ Courses Available
             </div>
         </div>
     )
