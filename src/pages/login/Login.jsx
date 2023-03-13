ed2b8f408a77da1dbd15f51177811212112e219a
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-03-14 00:01:41 +0530

    login Page UI

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,21 +4,26 @@ import "./login.scss"
 const Login=()=>{
     return(
         <div className="Login">
-            <div className="login_container">
-                <label htmlFor="email"><Person className="person"/> </label>
-                <input type="email" placeholder="Email" className="inpBox email"/>
-                <input type="password" placeholder="Password" className="inpBox password"/>
-                <label htmlFor="" id="rememberMe"><input type="checkbox" name="" id="chkbx" /> Remember Me</label>
-                <button className="sign_in">Sign In</button>
+            <div className="image flex_item"></div>
+            <div className="login_container flex_item">
+                <div className="login">
+                    {/* <label htmlFor="email">Email</label> */}
+                    <input type="email" name="" id="" placeholder="Enter Email Id"/>
+                    {/* <label htmlFor="password">Password</label> */}
+                    <input type="password" name="" id="" placeholder="Enter Password"/>
+                    <label htmlFor="" id="rememberMe"><input type="checkbox" name="" id="chkbx" /> Remember Me</label>
+                    <button className="sign_in">Sign In</button>
 
-                <div className="linkconatiner">
-                    <span>Register</span>
-                    <span>Forget password?</span>
-                </div>
-                <div className="icon_container">
-                    <Facebook className="Icon fb"/>
-                    <Twitter className="Icon twitter"/>
-                    <LinkedIn className="Icon twitter"/>
+                    <div className="link_conatiner">
+                        <span>Register</span>
+                        <span>Forget password?</span>
+                    </div>
+                    <div className="icon_container">
+                        <h6>Login With:</h6>
+                        <Facebook className="Icon fb"/>
+                        <Twitter className="Icon twitter"/>
+                        <LinkedIn className="Icon twitter"/>
+                    </div>
                 </div>
             </div>
         </div>
