4f2f828167e314a59daeba4ddf833e2915c5cca1
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-03-14 00:02:23 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.scss';
 import Home from "./pages/home/Home"
+import Login from './pages/login/Login';
 function App() {
   return (
     <div className="App">
-     <Home></Home>
+      <Login/>
     </div>
   );
 }
