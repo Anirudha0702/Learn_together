a71d6e9c89435cd4a09de25d40160b96a1f69c51
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-03-14 23:43:49 +0530

    Footer UI

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,36 +1,46 @@
-import { Copyright, Facebook, GitHub, Instagram, LinkedIn, Mail, Phone, Place } from "@material-ui/icons";
+import { SchoolSharp,Copyright, Facebook, GitHub, Instagram, LinkedIn, Mail, Phone, Place } from "@material-ui/icons";
+import {FaQuoteLeft,FaQuoteRight} from "react-icons/fa"
 import "./footer.scss"
-import logo from "../../Assets/logo.png"
 const Footer=()=>{
     return(
         <div className="Footer">
+            <div className="quote">
+                <span><FaQuoteLeft/>  It is the mark of an educated mind to be able to entertain a thought without accepting it <FaQuoteRight/></span>
+                <button className="btn">View All Courses</button>
+            </div>
             <div className="container">
-                <div className="logo flx">
-                    <img src={logo} alt="" />
-                    <span className="text">Learn Together</span><br></br>
-                    Established in the year 1995,Solid Tuition has been instrumental in numerous success stories. Tuition classes are really helpful for students to overcome their weaknesses .
-            </div>
-                <div className="contact flx">
-                    <h3>Contact:</h3>
-                    <Place className="Icon"/>Purba Putiary, Kolkata, West Bengal
-                    <Mail className="Icon"/>anurudhapradhan403@gmail.com
-                    <Phone className="Icon"/>7044585997
+                <div className="SiteDetails_Container">
+                    <div className="info grid_Item">
+                        <div className="logo">
+                        <SchoolSharp className="logoImg"/>
+                        <span className="text">Learn Together</span></div>
+                        <span>
+                        Established in the year 1995,Solid Tuition has been instrumental in numerous success stories. Tuition classes are really helpful for students to overcome their weaknesses .
+                        </span>
+                    </div>
+                    <div className="contact grid_Item">
+                        <h3>Contact:</h3>
+                        <span><Place className="Icon"/>Purba Putiary, Kolkata, West Bengal</span>
+                        <span><Mail className="Icon"/>anurudhapradhan403@gmail.com</span>
+                        <span><Phone className="Icon"/>7044585997</span>
+                    </div>
+                    <div className="feedback grid_Item">
+                        <h3>Feedback:</h3>
+                        <textarea name="" id="" cols="30" rows="10" placeholder="Write feedback"></textarea>
+                        <button type="submit">Submit</button>
+                    </div>
                 </div>
-                <div className="feedback flx">
-                    <h3>Feedback:</h3>
-                    <textarea name="" id="" cols="30" rows="10" placeholder="Write feedback"></textarea>
-                    <button type="submit">Submit</button>
+                <div className="developerDetails_Container">
+                    <Facebook className="Icon"/>
+                    <LinkedIn className="Icon"/>
+                    <GitHub className="Icon"/>
+                    <Instagram className="Icon"/>
+                </div>
+                <div className="copyright">
+                   
+                    <small> <Copyright/>Copyright <b> 2022-2030</b> All Rights Reserved. Created By <b>Anirudha pradhan</b></small>
                 </div>
             </div>
-            <div className="linkContainer">
-                <Facebook className="Icon"/>
-                <LinkedIn className="Icon"/>
-                <GitHub className="Icon"/>
-                <Instagram className="Icon"/>
-            </div>
-            <span><Copyright/> Copyright <b> 2022-2030</b></span>
-            <small>All Rights Reserved. Created By <b>Anirudha pradhan</b></small>
-
         </div>
     )
 }
