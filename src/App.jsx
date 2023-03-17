013ecb59ec0496401bb709eae84ec36bf96d1799
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-03-17 15:04:43 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 
 import './App.scss';
-import Footer from './components/footer/Footer';
-import Testimonial from './components/testimonial/Testimonial';
+import About from './pages/about/About';
 import Home from "./pages/home/Home"
-// import Login from './pages/login/Login';
+import Login from './pages/login/Login';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import User from './pages/user/User';
 function App() {
   return (
+    <>
+    <Router>
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      <Routes>
+        <Route path='/login' element={<User/>}/>
+      </Routes>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./navbar.scss"
 import profileImg from "../../Assets/people.png"
+import { Link } from "react-router-dom";
 import { AccountCircle, People, SchoolSharp } from "@material-ui/icons";
 const Navbar=()=>{
     const [clicked,setClick]=useState(false);
@@ -18,11 +19,11 @@ const Navbar=()=>{
                     <span className="line"></span>
                 </div>
                 <ol className={clicked?"navList click":"navList"}>
-                    <li className="item">Home</li>
+                    <li className="item"><Link className="Link" to={"/"}>Home</Link></li>
                     <li className="item">Courses</li>
-                    <li className="item">About</li>
+                    <li className="item"><Link className="Link" to={"/about"}>About</Link></li>
                     <li className="item">Contact Us</li>
-                    <li className="item"><div className="profile"><AccountCircle className="img"/></div></li>
+                    <li className="item"><Link className="Link" to ={"/login"}><div className="profile"><AccountCircle className="img"/></div></Link></li>
                 </ol>
             </div>
         </div>
