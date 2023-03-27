f39bdde6ff8d9d623153968ec87167197c7a7aa2
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-03-27 13:02:25 +0530

    userPageWithoutBigScreenResponsive

--- client/src/pages/user/User.jsx
+++ client/src/pages/user/User.jsx
@@ -1,32 +1,48 @@
+
 import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 import "./user.scss";
 import profile from "../../Assets/MYiMG.jpeg";
-import cover from "../../Assets/registerBg.jpg"
 import {AiTwotoneEdit} from "react-icons/ai";
-import {MdAddAPhoto} from "react-icons/md";
+import API from "../../API";
 const User=()=>{
-    // const course=API();
+    const courseArray=API();
     return(
         <div className="User">
             <div className="cover">
                 <Navbar/>
-                <div className="coverImg">
+                {/* <div className="coverImg">
                     <img src={cover} alt="" />
                     <MdAddAPhoto className="addCvr"/>
-                </div>
+                </div> */}
             </div>
             <div className="userContainer">
                 <div className="userInfo">
-                    <div className="details">
-                        <h1>Anirudha Pradhan <AiTwotoneEdit/></h1>
-                        <span>anirudhapradhan403@gmail.com</span>
-                        <span><h3>Bio</h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eveniet laborum deleniti excepturi voluptatem illum ipsum qui totam dolore animi delectus consequatur aliquam, quo sequi fugiat reiciendis soluta sunt iure!</span>
-                    </div>
                     <div className="profile">
                         <img src={profile} alt="" />
                     </div>
+                    <div className="details">
+                        <h3>Anirudha Pradhan <AiTwotoneEdit/></h3>
+                        <span>anirudhapradhan403@gmail.com</span>
+                        <span><h3>Bio</h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eveniet laborum deleniti excepturi voluptatem illum ipsum qui totam dolore animi delectus consequatur aliquam, quo sequi fugiat reiciendis soluta sunt iure!</span>
+                    </div>
+                </div>
+                <div className="coursesIn">
+                    {
+                        courseArray.map((course,key)=>{
+                            return(
+                                <div className="course">
+                                    <div className="img">
+                                        <img src={course.image} alt="" />
+                                    </div>
+                                    <span>{course.title}</span>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
+            <Footer/>
 	    </div>
     )
 }
