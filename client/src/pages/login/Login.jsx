2cae6e98bc5097514ce576be21f1e53f24493be5
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-04-19 16:32:09 +0530

    login-connect

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,19 +1,39 @@
 import {FaFacebook,FaLinkedin,FaInstagram} from "react-icons/fa"
-
+import {useState} from "react"
 import "./login.scss"
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
+import{ToastContainer,toast} from "react-toastify"
+import {findUser} from "../../API"
 const Login=()=>{
+    const navigate=useNavigate();
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const handleLogin=()=>{
+        const found=findUser({password,email});
+        if(found){
+            toast.success('logged in..');
+            setTimeout(() => {
+                navigate("/");
+            }, 3000);
+        }
+        else{
+            toast.error("Faild to login ");
+        }
+    }
     return(
         <div className="Login">
             <div className="image flex_item"></div>
             <div className="login_container flex_item">
                 <div className="login">
                     {/* <label htmlFor="email">Email</label> */}
-                    <input type="email" name="" id="" placeholder="Enter Email Id"/>
+                    <input type="email" name="" placeholder="Enter Email Id" onChange={(e)=>{
+                        setEmail(e.target.value)
+                    }}/>
                     {/* <label htmlFor="password">Password</label> */}
-                    <input type="password" name="" id="" placeholder="Enter Password"/>
-                    <label htmlFor="" id="rememberMe"><input type="checkbox" name="" id="chkbx" /> Remember Me</label>
-                    <button className="sign_in">Sign In</button>
+                    <input type="password" name=""  placeholder="Enter Password" onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }}/>
+                    <button className="sign_in" onClick={handleLogin}>Sign In</button>
 
                     <div className="link_conatiner">
                         <span><Link to={"/register"}>Register</Link> </span>
@@ -27,6 +47,18 @@ const Login=()=>{
                     </div>
                 </div>
             </div>
+            <ToastContainer
+            position="top-left"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+            />
         </div>
     )
 }
