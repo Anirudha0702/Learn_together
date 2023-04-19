bcb4a3d99c517e418beccbe60720f3f10fc8e2f4
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-04-19 15:47:27 +0530

    reStructure navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import "./navbar.scss"
 import { Link } from "react-router-dom";
 import {  FaSchool } from "react-icons/fa";
-import {RiAccountCircleFill} from "react-icons/ri"
+import {GiHamburgerMenu,GiTireIronCross} from "react-icons/gi"
+
+
 const Navbar=()=>{
     const [clicked,setClick]=useState(false);
-    const  handleOpen=()=>{
+    const  handleNavToggle=()=>{
         setClick(!clicked);
     }
+    const user=JSON.parse(localStorage.getItem("User"));
+    console.log(user);
     return( 
         <div className="Navbar">
             <div className="logo">
@@ -15,17 +19,20 @@ const Navbar=()=>{
                 <span className="text">Learn Together</span>
             </div>
             <div className="navItemContainer">
-                <div className={clicked?"hamburgerMenu clicked":"hamburgerMenu"} onClick={handleOpen}>
-                    <span className="line"></span>
+                <div className="hamburgerMenu" onClick={handleNavToggle}>
+                    <GiHamburgerMenu/>
+                    <GiTireIronCross/>
                 </div>
                 <ol className={clicked?"navList click":"navList"}>
                     <li className="item"><Link className="Link" to={"/"}>Home</Link></li>
                     <li className="item">Courses</li>
                     <li className="item"><Link className="Link" to={"/about"}>About</Link></li>
                     <li className="item">Contact Us</li>
-                    <li className="item"><Link className="Link" to ={"/login"}><div className="profile">
-                        <RiAccountCircleFill className="img"/>
-                        </div></Link></li>
+                    <li className="item"><Link className="Link" to ="/user">
+                        <div className="profile">
+                        <img src={user.profileImg} alt="dssgv" />
+                        </div>
+                        </Link></li>
                 </ol>
             </div>
         </div>
