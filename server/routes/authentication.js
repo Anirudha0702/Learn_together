54d93599c748dbd39273a5ec7630ca6a7679e61b
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-05-20 15:48:23 +0530

    auth update

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -13,16 +13,28 @@ router.post("/register",async (req,res)=>{
     try {
         const user=await NewUser.save()
         res.status(200).json(user);
+        console.log("success")
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
 })
 router.post("/login",async (req,res)=>{
+
     try {
-    const user=  await User.findOne({email:req.body.email});
-        user?res.status(200).json(user):res.status(404).json("eooro");
-        console.log(user+"success");
+        const user=  await User.findOne({email:req.body.email});
+        !user && res.status(404).json("user not found");
+        const passWord=Crypto.AES.decrypt(user.password,process.env.SECRET_KEY).toString(Crypto.enc.Utf8);
+         if(req.body.password===passWord){
+            res.status(201).json({
+                user:user.userName,
+                dp:user.profileImg,
+                email:user.email
+            });
+         }
+         else{
+            res.status(401).json("Wrong password");
+         }
     } catch (error) {
         res.status(500);
         console.log(error+"error");
